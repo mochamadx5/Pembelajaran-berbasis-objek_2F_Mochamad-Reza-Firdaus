41388f9cf1221a655da7a02144a923282117c401
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-18 00:24:28 +0700

    jobsheet3_tugasNo6

--- jobsheet3/Encap/TestKoperasi.java
+++ jobsheet3/Encap/TestKoperasi.java
@@ -1,26 +1,37 @@
 package jobsheet3.Encap;
 
+import java.util.Scanner;
+
 public class TestKoperasi {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Anggota donny = new Anggota("111333444", "Donny", 5000000);
 
         System.out.println("Nama Anggota: " + donny.getNama());
         System.out.println("Limit Pinjaman: " + donny.getLimitPeminjaman());
 
-        System.out.println("\nMeminjam uang 10.000.000...");
-        donny.pinjam(10000000);
+        // Meminjam uang
+        System.out.print("\nMasukkan nominal pinjaman pertama: ");
+        double pinjam1 = input.nextDouble();
+        donny.pinjam(pinjam1);
         System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPeminjaman());
 
-        System.out.println("\nMeminjam uang 4.000.000...");
-        donny.pinjam(4000000);
+        System.out.print("\nMasukkan nominal pinjaman kedua: ");
+        double pinjam2 = input.nextDouble();
+        donny.pinjam(pinjam2);
         System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPeminjaman());
 
-        System.out.println("\nMembayar angsuran 100.000");
-        donny.angsur(100000);
+        // Membayar angsuran
+        System.out.print("\nMasukkan nominal angsuran pertama: ");
+        double angsur1 = input.nextDouble();
+        donny.angsur(angsur1);
         System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPeminjaman());
 
-        System.out.println("\nMembayar angsuran 3.000.000");
-        donny.angsur(3000000);
+        System.out.print("\nMasukkan nominal angsuran kedua: ");
+        double angsur2 = input.nextDouble();
+        donny.angsur(angsur2);
         System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPeminjaman());
+
+        input.close();
     }
 }
