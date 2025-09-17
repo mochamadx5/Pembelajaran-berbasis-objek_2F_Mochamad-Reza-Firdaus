eb7656f2c2f78021d6001bd430bbff13e9e9eba5
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-17 19:49:13 +0700

    jobsheet3_percobaan3

---
+++ jobsheet3/KoperasiGetterSetter/KoperasiDemo.java
@@ -0,0 +1,14 @@
+package jobsheet3.KoperasiGetterSetter;
+
+public class KoperasiDemo {
+    public static void main(String[] args) {
+        Anggota ang1 = new Anggota();
+        ang1.setNama("Fikri Setiawan");
+        ang1.setAlamat("Jalan Uranus no 12");
+        ang1.setor(100000);
+        System.out.println("simpanan " + ang1.getNama() + " : Rp " + ang1.getSimpanan());
+        
+        ang1.pinjam(10000);
+        System.out.println("simpanan " + ang1.getNama() + " : Rp " + ang1.getSimpanan());
+    }
+}
