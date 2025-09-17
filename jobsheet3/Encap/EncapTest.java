84db3f0c734d00a84b870112ba01f6333719324a
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-17 23:38:47 +0700

    jobsheet3_tugas no 1

---
+++ jobsheet3/Encap/EncapTest.java
@@ -0,0 +1,12 @@
+package jobsheet3.Encap;
+
+public class EncapTest {
+    public static void main(String[] args) {
+        EncapDemo encap = new EncapDemo();
+        encap.setName("Yasir");
+        encap.setAge(35);
+
+        System.out.println("Name : " + encap.getName());
+        System.out.println("Age : " + encap.getAge());
+    }
+}
