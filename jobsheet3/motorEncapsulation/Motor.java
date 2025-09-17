a7958506170fdcaa5ee28ba0677c01f8a5478e01
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-17 19:11:01 +0700

    jobsheet3_praktikum 2

--- jobsheet3/motorEncapsulation/Motor.java
+++ jobsheet3/motorEncapsulation/Motor.java
@@ -2,9 +2,30 @@ package jobsheet3.motorEncapsulation;
 
     public class Motor{
 
-        public int kecepatan = 0;
-        public boolean kontakOn = false;
-
+        private int kecepatan = 0;
+        private boolean kontakOn = false;
+        
+        public void nyalakanMesin() {
+            kontakOn=true;
+        }
+        public void matikanMesin() {
+            kontakOn = false;
+            kecepatan = 0;
+        }
+        public void tambahKecepatan() {
+            if (kontakOn == true) {
+                kecepatan += 5;
+            } else {
+                System.out.println("kecepatan tidak bisa bertambah karena mesin off! \n");
+            }
+        }
+        public void kurangiKecepatan () {
+            if (kontakOn == true) {
+                kecepatan -= 5;
+            } else {
+                System.out.println("Kecepatan tidak bisa berkurang karena mesin Off! \n");
+            }
+        }
         public void printStatus () {
             if (kontakOn == true) {
                 System.out.println("kontak on");
