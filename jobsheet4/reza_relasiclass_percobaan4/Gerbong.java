a5e7198afb4d999a99a6061b5ac3dddefee9ebec
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-10-09 23:49:24 +0700

    Jobsheet4_percobaan4_pertanyaanNO5

--- jobsheet4/reza_relasiclass_percobaan4/Gerbong.java
+++ jobsheet4/reza_relasiclass_percobaan4/Gerbong.java
@@ -4,21 +4,21 @@ public class Gerbong {
     private String kode;
     private Kursi[] arrayKursi; // Array objek Kursi
 
-    // Method initKursi() 
+    // Method untuk inisialisasi semua kursi
     private void initKursi() { 
         for (int i = 0; i < arrayKursi.length; i++) {
             this.arrayKursi[i] = new Kursi(String.valueOf(i + 1));
         }
     }
 
-    // f. Constructor Gerbong
+    // Constructor Gerbong
     public Gerbong(String kode, int jumlah) {
         this.kode = kode;
-        this.arrayKursi = new Kursi[jumlah];
-        this.initKursi(); 
+        this.arrayKursi = new Kursi[jumlah]; // Alokasi memori array
+        this.initKursi(); // Panggil method untuk mengisi array
     }
 
-    // Setter dan Getter
+    // Getter dan Setter
     public void setKode(String kode) {
         this.kode = kode;
     }
@@ -27,20 +27,30 @@ public class Gerbong {
         return kode;
     }
     
-    public Kursi[] getArrayKursi() {
-        return arrayKursi;
-    }
-    
-    // Method setPenumpang()
+    // METHOD setPenumpang()
     public void setPenumpang(Penumpang penumpang, int nomor) {
-        this.arrayKursi[nomor - 1].setPenumpang(penumpang);
+        int indeks = nomor - 1; 
+        if (indeks < 0 || indeks >= arrayKursi.length) {
+            System.out.println("ERROR: Nomor kursi " + nomor + " tidak ada dalam gerbong ini!");
+            return;
+        }
+
+        // Cek apakah kursi sudah terisi
+        if (this.arrayKursi[indeks].getPenumpang() == null) {
+            // Jika kursi KOSONG, set penumpang
+            this.arrayKursi[indeks].setPenumpang(penumpang);
+            System.out.println("Penumpang " + penumpang.getNama() + " berhasil duduk di kursi " + nomor);
+        } else {
+            // Jika kursi SUDAH TERISI, berikan pesan error
+            System.out.println("ERROR: Kursi nomor " + nomor + " sudah terisi oleh penumpang lain!");
+        }
     }
 
     // Method info()
     public String info() {
         String info = "";
-        info += "Kode: " + kode + "\n"; 
-        // Loop for-each untuk menampilkan info setiap kursi dalam array
+        info += "Kode: " + kode + "\n";
+        
         for (Kursi kursi : arrayKursi) {
             info += kursi.info();
         }
