9ea5bf5ed0dddf26dd0ea7745bfbc094fcb7826e
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-10-24 22:50:51 +0700

    jobsheet2_percobaan2_pertanyaanD

--- jobsheet9/abstractclass/interfacelatihan/Rektor.java
+++ jobsheet9/abstractclass/interfacelatihan/Rektor.java
@@ -1,14 +1,18 @@
 package jobsheet9.abstractclass.interfacelatihan;
 
 public class Rektor {
-    public void beriSertifikatCumlaude(ICumlaude mahasiswa){
-        System.out.println("saya rektor, memberikan sertifikat cumlaude.");
-        System.out.println("Selamat! Silakan perkenalkan diri anda.");
+    public void beriSertifikatCumlaude(ICumlaude mahasiswa) {
+        System.out.println("Saya REKTOR, memberikan sertifikat cumlaude.");
+        System.out.println("Selamat! Bagaimana Anda bisa cumlaude?");
+
+        // Casting ke Mahasiswa agar bisa panggil kuliahDiKampus()
+        if (mahasiswa instanceof Mahasiswa) {
+            ((Mahasiswa) mahasiswa).kuliahDiKampus();
+        }
 
         mahasiswa.lulus();
         mahasiswa.meraihIPKTinggi();
 
         System.out.println("-------------------------------------------");
     }
-    
 }
